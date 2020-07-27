3123c96b08f4be69e697e708cbf31eb846bf49fd
Murshid Hassen <mh.murshidco.uk@outlook.com>
2020-07-27 12:47:22 +0530

    working reddit changes

---
+++ src/main/java/com/vimo/reddit/service/AuthService.java
@@ -0,0 +1,12 @@
+package com.vimo.reddit.service;
+
+import com.vimo.reddit.dto.RegisterRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+    public void signup(RegisterRequest registerRequest){
+        
+    }
+
+}
